7528ecd0d30441d016f78c1d3e43a6ea93354168
ETTRAN <70864132+ETTRAN777@users.noreply.github.com>
2025-05-18 23:41:27 -0400

    i forgor one line

--- backEnd.js
+++ backEnd.js
@@ -13,6 +13,10 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/home.html');
+});
+
 app.get('/users', async (req, res) => {
     console.log('Attempting to GET all users')
     const { data, error } = await supabaseClient.from('users').select();
